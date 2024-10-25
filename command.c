1615093957164153e9e0eea2c6ff2b3ef370deb1
mduy2003 <mduy2003@csu.fullerton.edu>
2024-10-25 16:58:25 -0700

    Added function

--- command.c
+++ command.c
@@ -1,1 +1,9 @@
 #include "command.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *execute_command(char *command, FILE *input_file)
+{
+
+}
--- command.h
+++ command.h
@@ -1,6 +1,8 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
+#include <stdio.h>
 
+char *execute_command(char *command, FILE *input_file);
 
 #endif // COMMAND_H
