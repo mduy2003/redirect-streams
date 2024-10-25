7db161c8bcf89233a37cac18b181f818724aac39
mduy2003 <mduy2003@csu.fullerton.edu>
2024-10-25 16:53:44 -0700

    Updated main

--- main.c
+++ main.c
@@ -2,6 +2,20 @@
 
 int main(int argc, char *argv[]) 
 {
-    printf("Hello, World!\n");
+    if (argc != 4)
+    {
+        printf("Usage: ./redir <input> <command> <output>\n");
+        return 1;
+    }
+
+    FILE *input_file = fopen(argv[1], "r");
+    char * command = argv[2];
+    FILE *output_file = fopen(argv[3], "w");
+
+    char * result = NULL;
+
+    fclose(input_file);
+    fprintf(output_file, "%s", result);
+    fclose(output_file);
     return 0;
 }
